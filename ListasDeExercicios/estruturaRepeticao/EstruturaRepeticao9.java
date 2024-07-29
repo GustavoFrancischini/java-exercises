8cd4be1747e4347a3657ad5c5559d0fc85ea3929
Gustavo <64756160+GustavoFrancischini@users.noreply.github.com>
2024-07-29 15:49:45 -0300

    Create EstruturaRepeticao9.java

---
+++ ListasDeExercicios/estruturaRepeticao/EstruturaRepeticao9.java
@@ -0,0 +1,21 @@
+package estruturaRepeticao;
+
+// Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+
+import java.util.*;
+
+public class EstruturaRepeticao9 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
