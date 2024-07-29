a4b80db5b1a4efe86b4445d4f1f6c51b5cc5d125
Gustavo <64756160+GustavoFrancischini@users.noreply.github.com>
2024-07-29 15:44:33 -0300

    Update EstruturaRepeticao5.java

--- ListasDeExercicios/estruturaRepeticao/EstruturaRepeticao5.java
+++ ListasDeExercicios/estruturaRepeticao/EstruturaRepeticao5.java
@@ -10,35 +10,39 @@ public class EstruturaRepeticao5 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Informe a população do país A: ");
+        System.out.println("Enter the population of country A: ");
         double countryA = sc.nextDouble();
-        System.out.println("Informe a população do país B: ");
+        System.out.println("Enter the growth rate of country A: ");
+        double rateA = sc.nextDouble();
+        System.out.println("Enter the population of country B: ");
         double countryB = sc.nextDouble();
-        System.out.println("Informe a taxa de crescimento do país A (em %): ");
-        double growthRateA = sc.nextDouble();
-        System.out.println("Informe a taxa de crescimento do país B (em %): ");
-        double growthRateB = sc.nextDouble();
-
-        int years = 0;
-
-        if (countryA < countryB) {
-            while (countryA < countryB) {
-                countryA += countryA * growthRateA / 100;
-                countryB += countryB * growthRateB / 100;
-                years++;
-            }
-            System.out.printf("O país A vai precisar de %d anos para ultrapassar o país B.%n", years);
-        } else if (countryB < countryA) {
-            while (countryB < countryA) {
-                countryA += countryA * growthRateA / 100;
-                countryB += countryB * growthRateB / 100;
-                years++;
-            }
-            System.out.printf("O país B vai precisar de %d anos para ultrapassar o país A.%n", years);
-        } else {
-            System.out.println("A população dos países é igual.");
+        System.out.println("Enter the growth rate of country B: ");
+        double rateB = sc.nextDouble();
+        
+        int year = 0;
+        
+        if(countryA < 0 || countryB < 0 || rateA < 0 || rateB < 0) {
+            System.out.println("Enter valid values.");
+            System.exit(0);
         }
-
+        if (countryA < countryB && rateA > rateB) {
+            while (countryA < countryB) {
+                year += 1;
+                countryA += (rateA / 100) * countryA;
+                countryB += (rateB / 100) * countryB;
+            }
+            System.out.printf("It took %d years for the population of country A to surpass country B.\n", year);
+        } else if (countryA > countryB && rateA < rateB) {
+            while (countryA > countryB) {
+                year += 1;
+                countryA += (rateA / 100) * countryA;
+                countryB += (rateB / 100) * countryB;
+            }
+            System.out.printf("It took %d years for the population of country B to surpass country A.\n", year);
+        } else {
+            System.out.println("The countries have the same population, the same growth rate, or the country with the smaller population has a lower growth rate. \nTherefore, the population of the country with the smaller population will never surpass the other country's.");
+        }
+        
         sc.close();
     }
 }
