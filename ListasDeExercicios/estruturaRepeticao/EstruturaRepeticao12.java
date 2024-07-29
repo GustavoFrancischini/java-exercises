f150ead8aee135dfdda5d6495ee4fc0b9e34215c
Gustavo <64756160+GustavoFrancischini@users.noreply.github.com>
2024-07-29 15:52:36 -0300

    Create EstruturaRepeticao12.java

---
+++ ListasDeExercicios/estruturaRepeticao/EstruturaRepeticao12.java
@@ -0,0 +1,25 @@
+package estruturaRepeticao;
+
+/*
+
+Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+O usuário deve informar de qual numero ele deseja ver a tabuada.
+
+ */
+
+import java.util.*;
+
+public class EstruturaRepeticao12 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        System.out.println("Type a number: ");
+        int num = sc.nextInt();
+
+        for (int i : list) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
