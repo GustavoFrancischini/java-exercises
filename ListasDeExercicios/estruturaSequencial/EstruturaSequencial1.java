43f5cdacdc82cca9ed10f8edccf2e441aa660df5
GustavoFrancischini <gustavo.gofran@gmail.com>
2025-09-30 23:01:58 -0300

    oi

--- ListasDeExercicios/estruturaSequencial/EstruturaSequencial1.java
+++ ListasDeExercicios/estruturaSequencial/EstruturaSequencial1.java
@@ -3,7 +3,7 @@ package estruturaSequencial;
 // Faça um Programa que mostre a mensagem "Alô, mundo!" na tela.
 
 public class EstruturaSequencial1 {
-
+	
 	public static void main(String[] args) {
 		System.out.println("Alô, mundo!");
 	}
